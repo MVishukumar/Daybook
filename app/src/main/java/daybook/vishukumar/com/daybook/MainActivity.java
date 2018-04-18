0ec887fd8bc3dac91d471600a2c14be14c2593f2
vishukumar <beingvishu@gmail.com>
2018-04-18 12:26:24 +0530

    Initial commit

---
+++ app/src/main/java/daybook/vishukumar/com/daybook/MainActivity.java
@@ -0,0 +1,13 @@
+package daybook.vishukumar.com.daybook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
